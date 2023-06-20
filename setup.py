0b2eeca0ff9c44fc5543135209366f010ced6ed2
Liam Bailey <liam.bailey@skillcorner.com>
2023-06-20 09:53:34 +0100

    Updated font

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     packages=['skillcorner_analysis_toolkit',
               'skillcorner_analysis_toolkit/resources'],
 
-    package_data={'': ['skillcorner_analysis_toolkit/resources/Montserrat-VariableFont_wght.ttf']},
+    package_data={'font': ['skillcorner_analysis_toolkit/resources/Montserrat-VariableFont_wght.ttf']},
+
     include_package_data=True,
     install_requires=['adjustText==0.7.3',
                       'matplotlib==3.7.0',
